603a81fe09d3b0d810c8c522191e2fa8472febda
Dipali <dipssonawane123@gmail.com>
2022-06-21 08:40:22 +0530

    Added: program$ added

---
+++ src/com/program/Program4.java
@@ -0,0 +1,17 @@
+package com.program;
+
+public class Program4 {
+    public static void main(String args[]) {
+        int P = Integer.parseInt(args[0]);
+        int Y = Integer.parseInt(args[1]);
+        int R = Integer.parseInt(args[2]);
+        monthlyPayment(P, Y, R);
+    }
+
+    public static void monthlyPayment(int P, int Y, double R) {
+        double n = 12 * Y;
+        double r = R / (12 * 100);
+        double payment = (P * r) / (1 - Math.pow((1 + r), -n));
+        System.out.println("MonthlyPayment:" + payment);
+    }
+}
